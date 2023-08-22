4a70bc0450f3a01140c451bf74695f26d569efbd
santosvini7 <vini_santos7@live.com>
2023-08-22 20:58:10 -0300

    :bricks: ci: criando CI com shard e docker images

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,20 +12,20 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     //  Base URL to use in actions like `await page.goto('/')`
      baseURL: 'https://www.saucedemo.com/',
-     headless: true,
+     headless: false,
      viewport: {
       width: 1280,
       height: 720
@@ -44,16 +44,20 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
+    {
+      name: 'Microsoft Edge',
+      use: { ...devices['Desktop Edge']}
+    },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
