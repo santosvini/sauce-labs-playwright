e5669858fcb52e866b7cfdfeebf485eec76d55a9
Vinicius dos Santos <46813491+santosvini@users.noreply.github.com>
2023-08-23 15:09:20 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   use: {
     //  Base URL to use in actions like `await page.goto('/')`
      baseURL: 'https://www.saucedemo.com/',
-     headless: false,
+     headless: true,
      viewport: {
       width: 1280,
       height: 720
